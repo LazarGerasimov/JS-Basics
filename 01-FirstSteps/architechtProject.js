a3061fbb9e96af659287761ee0a4b8828c5c1b37
Laz <lazgerasimov@gmail.com>
2021-10-02 13:55:24 +0100

    Adding 2 problems

---
+++ 01-FirstSteps/architechtProject.js
@@ -0,0 +1,10 @@
+    function archProject(input){
+        let name = (input[0])
+        let projects = Number(input[1])
+        let time = projects * 3
+
+        console.log(`The architect ${name} will need ${time} hours to complete ${projects} project/s.`)
+        
+     }
+
+    archProject(["George", "4"])
