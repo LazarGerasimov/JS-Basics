4d2bf6019046ce541195398afd1688964946f8a4
Laz <lazgerasimov@gmail.com>
2021-10-05 14:35:26 +0100

    Adding zooshop.js

---
+++ 01-FirstSteps/zooshop.js
@@ -0,0 +1,9 @@
+    function zooshop(input){
+        let dogFood = 2.50 * Number(input[0]);
+        let catFood = 4 * Number(input[1]);
+        let total = dogFood + catFood;
+        console.log(`${total} lv.`);
+       
+
+    }
+    zooshop(["5", "4"])
