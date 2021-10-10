b515915b19c9c0d6d770637c13362a20f7e0afd2
Laz <lazgerasimov@gmail.com>
2021-10-10 14:56:35 +0100

    speedInfo.js

---
+++ 02-ConditionalStatements/speedInfo.js
@@ -0,0 +1,18 @@
+function solve(input) {
+
+    let a = Number(input[0]);
+
+    if (a<=10){
+        console.log("slow")
+    } else if (a<=50) {
+        console.log("average")
+    } else if (a<=150) {
+        console.log("fast")
+    } else if (a<=1000) {
+        console.log("ultra fast")
+    } else {
+        console.log("extremely fast")
+    }
+
+}
+solve(["5120"])
