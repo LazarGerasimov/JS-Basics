b154efcfe6daae601a98bb20a8460737b420a383
Laz <lazgerasimov@gmail.com>
2021-10-29 15:22:30 +0100

    eve

---
+++ 04-ForLoops/evenPowersOf2.js
@@ -0,0 +1,11 @@
+function solve(input) {
+
+    let n = Number(input[0]);
+
+    for ( let i = 0; i <= n; i+=2) {
+        let result = Math.pow(2,i); 
+        console.log(result)
+    }
+
+}
+solve(["10"])
