64341ba7346e63c8cf90bebeec1ed40335ea43a4
Laz <lazgerasimov@gmail.com>
2021-10-30 15:26:27 +0100

    vowelsSum.js

---
+++ 04-ForLoops/vowelsSum.js
@@ -0,0 +1,20 @@
+function vowels (input) {
+
+    let text = input[0];
+    let result = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i]
+        switch (char) {
+            case "a": result +=1; break
+            case "e": result +=2; break
+            case "i": result +=3; break
+            case "o": result +=4; break
+            case "u": result +=5; break
+        }
+        
+    }
+    console.log(result)
+
+}
+vowels(["bamboo"]) 
