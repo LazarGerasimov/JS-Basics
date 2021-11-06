70818d85eafc0be55de1d5ba05d9049009d2b55c
Laz <lazgerasimov@gmail.com>
2021-11-06 15:32:50 +0000

    sequence2k+1.js

---
+++ 05-WhileLoops/sequence2k+1.js
@@ -0,0 +1,17 @@
+function solve(input) {
+
+    let index = 0;
+    let n = Number(input[index]);
+    index++;
+
+    let k = 1;
+
+    while (k <= n) {
+        console.log(k);
+        k = k * 2 + 1;
+
+    }
+
+
+}
+solve(["31"])
